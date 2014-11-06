7e877537fda865a0866f3d8d491e86e0de32c448
espiler <monkeysrawesome@gmail.com>
2014-11-05 17:15:37 -0800

    speedy

--- src/solvers.js
+++ src/solvers.js
@@ -14,37 +14,130 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var solution = new Board({n:n}); //fixme
+  // place a rook at 0,0
+  // row++, col++
+  var row = 0;
+  var col = 0;
+  while (row < n) {
+    solution.togglePiece(row, col);
+    row++;
+    col++;
+  }
+  return solution.rows();
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var count = 0;
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+
+  (function sub(n, board, currentRow){
+    var board = board || new Board({n:n});
+    var currentRow = currentRow || 0;
+    var pos;
+    // if current row is n
+    if (currentRow === n) {
+      count++;
+    } else {
+      for (var col = 0; col < n; col++) {
+        board.togglePiece(currentRow, col);
+        if (!board.hasRowConflictAt(currentRow) && !board.hasColConflictAt(col)) {
+          sub(n, new Board(board.rows()), currentRow + 1);
+        }
+        board.togglePiece(currentRow, col);
+      }
+    }
+
+  })(n);
+
+  return count;
 };
 
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+// window.findNQueensSolution = function(n) {
+//   var result = null;
+//   var board = new Board({n:n});
+//   var currentRow = 0;
+//   var startCol = 0;
+//   while (currentRow < n) {
+//     for (var col = (currentRow === 0 ? startCol : 0); col < n; col++) {
+//       board.togglePiece(currentRow, col);
+//       if (board.hasAnyQueenConflictsOn(currentRow,col)) {
+//         board.togglePiece(currentRow, col);
+//         if (col === n - 1) {
+//           break;
+//         }
+//       } else {
+//         break;
+//       }
+//     }
+//     if (col === n - 1) {
+//       startCol++;
+//       var board = new Board({n:n});
+//       break; //and somehow restart the loop
+//     }
+//     currentRow++;
+//   }
+//   return board.rows();
+// };
+//
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+window.findNQueensSolution = function(n) {
+  if (n === 2 || n === 3 ) {
+    return (new Board({n:n})).rows();
+  }
+  return(function sub(n, board, currentRow){
+    var board = board || new Board({n:n});
+    var currentRow = currentRow || 0;
+    // if current row is n
+    board.printBoard();
+    if (currentRow === n) {
+      return board.rows();
+    } else {
+      for (var col = 0; col < n; col++) {
+        board.togglePiece(currentRow, col);
+        if (!board.hasAnyQueenConflictsOn(currentRow,col)) {
+          var solution = sub(n, new Board(board.rows()), currentRow + 1);
+          if (solution) {
+            return solution;
+          }
+        }
+        board.togglePiece(currentRow, col);
+      }
+    }
+
+  })(n);
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var count = 0;
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+
+  (function sub(n, board, currentRow){
+    var board = board || new Board({n:n});
+    var currentRow = currentRow || 0;
+    // if current row is n
+    if (currentRow === n) {
+      count++;
+    } else {
+      for (var col = 0; col < n; col++) {
+        board.togglePiece(currentRow, col);
+        if (!board.hasAnyQueenConflictsOn(currentRow,col)) {
+          sub(n, new Board(board.rows()), currentRow + 1);
+        }
+        board.togglePiece(currentRow, col);
+      }
+    }
+
+  })(n);
+
+  return count;
+
 };
