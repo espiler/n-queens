85914797f1befdb91d158bb7d2ff9bae3a861bf3
espiler <monkeysrawesome@gmail.com>
2014-11-06 17:32:25 -0800

    worker init

--- worker.js
+++ worker.js
@@ -1,72 +1,48 @@
 var results = [];
 var initialCol = null;
 var n = null;
+var majDiag = [];
+var minDiag = [];
+var columns = [];
+var setupDone = false;
 
-function resultReceiver(event) {
-  results.push(parseInt(event.data));
-  if (results.length == 2) {
-    postMessage(results[0] + results[1]);
-  }
-}
-
-function errorReceiver(event) {
-  throw event.data;
-}
-
-onmessage = function(event) {
-  // i or start
+var onmessage = function (event) {
   if (event.data === 'start') {
     countNQueensSolutions();
   } else {
     var data = JSON.parse(event.data);
     initialCol = data.initialCol;
     n = data.n;
-
+    setUp();
   }
+}
+
+function countNQueensSolutions () {
+  (function tryNext (row) {
+    if (row === n) {
+      postmessage('yay');
+      return;
+    }
+
+    for (var col = 1; col < n; col++){
+      if (!columns[col] && !majDiag[col-row+n] && !minDiag[col+row]) {
+        columns[col] = majDiag[col-row+n] = minDiag[col+row] = 1;
+        tryNext(row + 1);
+        columns[col] = majDiag[col-row+n] = minDiag[col+row] = 0;
+      }
+    }
+  })(1);
+
 };
 
-function countNQueensSolutions (n) {
-  var count = 0;
-  var majDiag = [];
-  var minDiag = [];
+function setUp() {
   for (var i = 0; i < (2*n); i++) {
     minDiag[i] = 0;
     majDiag[i] = 0;
   }
-  var columns = [];
   for (var i = 0; i < n; i++) {
     columns[i] = 0;
   }
-
-  (function tryNext (row) {
-    if (row === n) {
-      count++;
-      return;
-    }
-    if (row === 0) {
-      for (var col = 0; col < (n - 1) / 2; col++){
-        columns[col] = majDiag[col+n] = minDiag[col] = 1;
-        tryNext(1);
-        columns[col] = majDiag[col+n] = minDiag[col] = 0;
-      }
-      count *= 2;
-      if (n % 2) {
-        col = Math.floor(n / 2);
-        columns[col] = majDiag[col+n] = minDiag[col] = 1;
-        tryNext(1);
-        columns[col] = majDiag[col+n] = minDiag[col] = 0;
-      }
-    } else {
-      for (var col = 0; col < n; col++){
-        if (!columns[col] && !majDiag[col-row+n] && !minDiag[col+row]) {
-          columns[col] = majDiag[col-row+n] = minDiag[col+row] = 1;
-          tryNext(row + 1);
-          columns[col] = majDiag[col-row+n] = minDiag[col+row] = 0;
-        }
-      }
-    }
-  })(0);
-
-  return count;
-
-};
+  columns[initialCol] = majDiag[initialCol+n] = minDiag[initialCol] = 1;
+  setupDone = true;
+}
